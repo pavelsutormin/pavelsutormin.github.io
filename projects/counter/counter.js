8ff982934ea74610839ca17fb429d3835d716b87
pasha <pavel.r.sutormin@gmail.com>
2026-05-10 21:01:31 +0200

    new marblerun_c version

--- projects/counter/counter.js
+++ projects/counter/counter.js
@@ -5,31 +5,47 @@ window.onload = function(e) {
 
   let count = 0;
 
+  const socket = io("https://api.sutormin.org");
+
   function changeCount(newCount) {
-    count = newCount;
+    count = Number(newCount);
     countElem.innerHTML = `count is ${count}`;
   }
 
   function addCount() {
-    fetch('https://api.sutormin.org/count/add')
+    /*fetch('https://api.sutormin.org/count/add')
       .then(response => {
         if (!response.ok) throw new Error('Network response was not ok');
         return response.text();
       })
       .then(data => changeCount(data))
-      .catch(error => console.error('Fetch error:', error));
+      .catch(error => console.error('Fetch error:', error));*/
+    socket.emit("count_add");
   }
 
   function subCount() {
-    fetch('https://api.sutormin.org/count/sub')
+    /*fetch('https://api.sutormin.org/count/sub')
       .then(response => {
         if (!response.ok) throw new Error('Network response was not ok');
         return response.text();
       })
       .then(data => changeCount(data))
-      .catch(error => console.error('Fetch error:', error));
+      .catch(error => console.error('Fetch error:', error));*/
+    socket.emit("count_sub");
   }
 
   counterAdd.addEventListener('click', () => addCount());
   counterSub.addEventListener('click', () => subCount());
+
+  socket.on("count_changed", (data) => {
+    changeCount(data);
+  });
+
+  fetch('https://api.sutormin.org/count/get')
+    .then(response => {
+      if (!response.ok) throw new Error('Network response was not ok');
+      return response.text();
+    })
+    .then(data => changeCount(data))
+    .catch(error => console.error('Fetch error:', error));
 }
